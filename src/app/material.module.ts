ebaa336a2eca5ab63d6bf9c345887b9442c26813
lenn <lenn_3k@hotmail.com>
2020-01-30 21:44:09 +0100

    Update angular/cdk and angular/material

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,11 @@
 import {NgModule} from '@angular/core';
-import {
-    MatButtonModule,
-    MatCheckboxModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatInputModule,
-    MatOptionModule,
-    MatSelectModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatOptionModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
     imports: [
