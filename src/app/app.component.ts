3b19d666a87fe2df279df62d240a4ef2fcb0a5c5
lenn <lenn_3k@hotmail.com>
2020-01-30 23:23:59 +0100

    rework timesheet to include shift time and overtime indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,85 +1,111 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-    title = 'Frogsheet';
+  title = 'Frogsheet';
 
-    dayInputs: DayInput[] = [];
-    todoWorkingHours: number;
+  dayInputs: Array<DayInput> = [];
+  todoWorkingHours: number;
+  currentMonth: number = new Date().getMonth();
+  currentYear: number = new Date().getFullYear();
 
-    ngOnInit(): void {
+  workingHours = 8.75;
+  maxShiftHours = 1.6;
 
-        const days = this.getDaysInMonth(new Date().getMonth(), new Date().getFullYear());
-        days.forEach((day: Date) => {
-            this.dayInputs.push({
-                date: day,
-                month: day.getMonth(),
-                day: day.getDate(),
-                startTime: '',
-                endTime: '',
-                breakTime: '',
-                type: 'work',
-                weekday: day.getDay() !== 0 && day.getDay() !== 6,
-                total: ''
-            });
-        });
+  monthList: Array<string> = new Array(12)
+    .fill(0)
+    .map((_, i) => i)
+    .map(i =>
+      moment()
+        .month(i)
+        .format('MMMM')
+    );
 
+  ngOnInit(): void {
+    this.updateDays();
+  }
 
+  calculateTotal(dayInput: DayInput): void {
+    let total: moment.Moment;
 
-        // this.todoWorkingHours = this.dayInputs
+    const startTime = moment(dayInput.startTime, 'HH:mm');
+    const endTime = moment(dayInput.endTime, 'HH:mm');
+    if (
+      startTime.isValid() &&
+      endTime.isValid() &&
+      startTime.isBefore(endTime)
+    ) {
+      total = endTime
+        .subtract(startTime.get('hour'), 'hour')
+        .subtract(startTime.get('minute'), 'minute');
 
+      dayInput.total = total.format('HH:mm');
 
+      dayInput = this.calculateShift(dayInput);
+
+      return;
+    }
+    dayInput.total = '';
+  }
+
+  updateDays(): void {
+    const days = this.getDaysInMonth(this.currentMonth, this.currentYear);
+    days.forEach((day: Date) => {
+      this.dayInputs.push({
+        date: day,
+        month: day.getMonth(),
+        day: day.getDate(),
+        startTime: '',
+        endTime: '',
+        weekday: day.getDay() !== 0 && day.getDay() !== 6,
+        total: ''
+      });
+    });
+  }
+
+  calculateShift(dayInput: DayInput): DayInput {
+    const total = moment(dayInput.total, 'HH:mm');
+
+    const totalNumber = total.get('hour') + total.get('minute') / 60;
+
+    const shiftNumber = totalNumber - this.workingHours;
+
+    const shift = moment()
+      .set('h', Math.abs(shiftNumber))
+      .format('HH:mm');
+
+    console.log(shift);
+
+    const newDayInput = { ...dayInput, shift, overtime: shiftNumber > 0 };
+
+    return newDayInput;
+  }
+
+  private getDaysInMonth(month: number, year: number): Array<Date> {
+    const date = new Date(year, month, 1);
+    const days = [];
+    while (date.getMonth() === month) {
+      days.push(new Date(date));
+      date.setDate(date.getDate() + 1);
     }
 
-    private getDaysInMonth(month, year) {
-        const date = new Date(year, month, 1);
-        const days = [];
-        while (date.getMonth() === month) {
-            days.push(new Date(date));
-            date.setDate(date.getDate() + 1);
-        }
-        return days;
-    }
-
-    calculateTotal(dayInput: DayInput) {
-        let total: moment.Moment;
-
-        const startTime = moment(dayInput.startTime, 'HH:mm');
-        const endTime = moment(dayInput.endTime, 'HH:mm');
-        if (startTime.isValid() && endTime.isValid() && startTime.isBefore(endTime)) {
-
-            total = endTime
-                .subtract(startTime.get('hour'), 'hour')
-                .subtract(startTime.get('minute'), 'minute');
-
-            const breakTime = moment(dayInput.breakTime, 'HH:mm');
-            if (breakTime.isValid()) {
-                total = total
-                    .subtract(breakTime.get('hour'), 'hour')
-                    .subtract(breakTime.get('minute'), 'minute');
-            }
-
-            dayInput.total = total.format('HH:mm');
-            return;
-        }
-        dayInput.total = '';
-
-    }
+    return days;
+  }
 }
 
 interface DayInput {
-    date?: Date;
-    month?: number;
-    day?: number;
-    startTime?: string;
-    endTime?: string;
-    breakTime?: string;
-    type?: string;
-    weekday?: boolean;
-    total?: string;
+  date?: Date;
+  month?: number;
+  day?: number;
+  startTime?: string;
+  endTime?: string;
+  weekday?: boolean;
+  total?: string;
+  shift?: string;
+  overtime?: boolean;
 }
